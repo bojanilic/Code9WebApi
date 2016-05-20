5ed229b28a51bbe052c3818d44cbf9dbf707377f
Bojan Ilic <b.ilic@levi9.com>
2016-05-21 01:52:50 +0200

    Initial commit

---
+++ Code9WebApi/DAL/CountryContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Code9WebApi.Models;
+
+namespace Code9WebApi.DAL
+{
+    public class CountryContext : DbContext
+    {
+        public CountryContext() : base("CountryContext")
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+        }
+    }
+}
