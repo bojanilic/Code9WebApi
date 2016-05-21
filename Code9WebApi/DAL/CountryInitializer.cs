d0c1fcb41bbc3eb87f88dbbb289ecda5ef59c551
Bojan Ilic <b.ilic@levi9.com>
2016-05-21 12:17:58 +0200

    connected to azure sql db

--- Code9WebApi/DAL/CountryInitializer.cs
+++ Code9WebApi/DAL/CountryInitializer.cs
@@ -4,36 +4,36 @@ using Code9WebApi.Models;
 
 namespace Code9WebApi.DAL
 {
-    public class CountryInitializer : DropCreateDatabaseAlways<CountryContext>
-    {
-        protected override void Seed(CountryContext context)
-        {
-            var countries = new List<Country>
-            {
-                new Country
-                {
-                    Name = "Serbia",
-                    Area = 77474,
-                    Currency = "Serbian dinar",
-                    ShortName = "RS"
-                },
-                new Country
-                {
-                    Name = "Brasil",
-                    Area = 8515767d,
-                    Currency = "real",
-                    ShortName = "BR"
-                },
-                new Country
-                {
-                    Name = "Japan",
-                    Area = 377972.28,
-                    Currency = "Yen",
-                    ShortName = "JP"
-                }
-            };
-            countries.ForEach(country => context.Countries.Add(country));
-            context.SaveChanges();
-        }
-    }
+    //public class CountryInitializer : DropCreateDatabaseAlways<CountryContext>
+    //{
+    //    protected override void Seed(CountryContext context)
+    //    {
+    //        var countries = new List<Country>
+    //        {
+    //            new Country
+    //            {
+    //                Name = "Serbia",
+    //                Area = 77474,
+    //                Currency = "Serbian dinar",
+    //                ShortName = "RS"
+    //            },
+    //            new Country
+    //            {
+    //                Name = "Brasil",
+    //                Area = 8515767d,
+    //                Currency = "real",
+    //                ShortName = "BR"
+    //            },
+    //            new Country
+    //            {
+    //                Name = "Japan",
+    //                Area = 377972.28,
+    //                Currency = "Yen",
+    //                ShortName = "JP"
+    //            }
+    //        };
+    //        countries.ForEach(country => context.Countries.Add(country));
+    //        context.SaveChanges();
+    //    }
+    //}
 }
